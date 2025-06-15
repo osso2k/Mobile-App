68670a2c7ab95236e8b14b2b7c899f2bbbecf9f9
osso2k <ossoble817@gmal.com>
2025-06-15 22:04:05 +0300

    new

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -2,15 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>PitchReels</Text>
-      <Text>HEY</Text>
+    <View className="flex-1 items-center pt-2">
+      <Text className="text-5xl font-bold text-accent ">PitchReels</Text>
     </View>
   );
 }
