fa9e69697b2b1b3fb479b597c7ad2569141b1637
osso2k <ossoble817@gmal.com>
2025-06-16 11:21:50 +0300

    new1

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,9 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center pt-2">
-      <Text className="text-5xl font-bold text-accent ">PitchReels</Text>
+      <Text className="text-5xl font-bold text-accent ">Movies</Text>
+      <Link href="/onboarding" className="text-blue-500 mt-4"> Onboarding</Link>
+      <Link href="/movie/avengers" className="text-blue-500 mt-4">
+        aveengers movie
+      </Link>
     </View>
   );
 }
